6a305b78a58a899a8a7ae597b7739478f5aa63dc
Jake Gaylor <jgaylor@fsw.com>
2018-10-06 00:26:26 -0600

    make the docker build process install dependencies

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM golang:1.10 as builder
 ARG HELM_CHART
 ARG API_VERSION
 ARG KIND
+RUN curl -fsSL -o /usr/local/bin/dep https://github.com/golang/dep/releases/download/v0.5.0/dep-linux-amd64 && chmod +x /usr/local/bin/dep
 WORKDIR /go/src/github.com/operator-framework/helm-app-operator-kit/helm-app-operator
 COPY helm-app-operator .
+RUN dep ensure -v
 RUN CGO_ENABLED=0 GOOS=linux go build -o bin/operator cmd/helm-app-operator/main.go
 RUN chmod +x bin/operator
 
@@ -15,7 +17,7 @@ ENV API_VERSION $API_VERSION
 ENV KIND $KIND
 WORKDIR /
 COPY --from=builder /go/src/github.com/operator-framework/helm-app-operator-kit/helm-app-operator/bin/operator /operator
-COPY $HELM_CHART /chart.tgz 
+COPY $HELM_CHART /chart.tgz
 RUN mkdir /chart \
   && tar -xzf /chart.tgz --strip-components=1 -C /chart \
   && rm /chart.tgz
--- release.sh
+++ release.sh
@@ -2,20 +2,24 @@
 
 set -e
 
-PROJECT=cloudflinger-com-prod
+PROJECT=cloudflinger
 LOCAL_IMAGE_NAME="openvpn-operator"
-REMOTE_IMAGE_NAME="gcr.io/${PROJECT}/${LOCAL_IMAGE_NAME}"
+REMOTE_IMAGE_NAME="${PROJECT}/${LOCAL_IMAGE_NAME}"
 
 if [ ! -f ./VERSION ]; then
 	echo "Please run this from the directory with the Dockerfile."
 fi
 
 VERSION=$(cat VERSION)
-cp ../../chart-releases/openvpn-$VERSION.tgz ./
+cp ../infrastructure/chart-releases/openvpn-$VERSION.tgz ./
 
 echo "Building version ${VERSION}."
 
-docker build -t $LOCAL_IMAGE_NAME --build-arg HELM_CHART=openvpn-$VERSION.tgz --build-arg API_VERSION=brain.cloudflinger.com/v1alpha1 --build-arg KIND=Openvpn .
+docker build -t $LOCAL_IMAGE_NAME \
+       --build-arg HELM_CHART=openvpn-$VERSION.tgz \
+       --build-arg API_VERSION=brain.cloudflinger.com/v1alpha1 \
+       --build-arg KIND=Openvpn \
+       .
 
 docker tag $LOCAL_IMAGE_NAME $REMOTE_IMAGE_NAME
 docker tag $LOCAL_IMAGE_NAME $REMOTE_IMAGE_NAME:latest
